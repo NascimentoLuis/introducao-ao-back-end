b85d150c712c04b97918f046d5ce51fdd8ddbcbf
Luís Henrique <91296970+NascimentoLuis@users.noreply.github.com>
2024-10-05 14:44:30 +0000

    adicionar calculadora php

---
+++ .devcontainer/calculadora.php
@@ -0,0 +1,10 @@
+<h1>Calculadora fullstack!</h1>
+    <form action="calculadora.php" method="post">    
+        <label>Numero 1</label>
+        <input type="text" name="numero1" id="numero1"></input>
+
+        <label>Numero 2</label>
+        <input type="text" name="numero2" id="numero2"></input>
+
+        <input type="submit" value="Calcular">
+    </form>
